8c58722dbae8cc23565bdd7a5ba937e2d72cac23
Robin Wieruch <wrobin@gmx.net>
2017-08-31 17:59:39 +0200

    add local state

--- src/components/SearchStories.js
+++ src/components/SearchStories.js
@@ -1,43 +1,39 @@
 import React, { Component } from 'react';
-import { inject, observer } from 'mobx-react';
+import { observable, action } from 'mobx';
+import { observer, inject } from 'mobx-react';
 import Button from './Button';
 import { fetchStories } from '../api/story';
 
-const applyQueryState = query => () => ({
-  query
-});
-
 @inject('storyStore') @observer
 class SearchStories extends Component {
+  @observable query = '';
+
   constructor(props) {
     super(props);
-
-    this.state = {
-      query: '',
-    };
 
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  @action
   onSubmit(event) {
     const { storyStore } = this.props;
-    const { query } = this.state;
 
-    if (query) {
-      fetchStories(query)
+    if (this.query) {
+      fetchStories(this.query)
         .then(result => storyStore.setStories(result.hits))
         .catch(error => storyStore.setError(error));
 
-      this.setState(applyQueryState(''));
+      this.query = '';
     }
 
     event.preventDefault();
   }
 
+  @action
   onChange(event) {
     const { value } = event.target;
-    this.setState(applyQueryState(value));
+    this.query = value;;
   }
 
   render() {
@@ -45,7 +41,7 @@ class SearchStories extends Component {
       <form onSubmit={this.onSubmit}>
         <input
           type="text"
-          value={this.state.query}
+          value={this.query}
           onChange={this.onChange}
         />
         <Button type="submit">
