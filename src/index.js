650710d7ff12b2cf6d26c449c2e9402ca781b68f
Robin Wieruch <wrobin@gmx.net>
2017-09-01 16:07:18 +0200

    part 09

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { autorun } from 'mobx';
 import './index.css';
 import App from './components/App';
 import store from './stores';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <App
-    stories={store.storyStore.readableStories}
-    onArchive={(objectID) => store.archiveStore.archivedStoryIds.push(objectID)}
-  />,
-  document.getElementById('root')
-);
+function render() {
+  ReactDOM.render(
+    <App
+      stories={store.storyStore.readableStories}
+      onArchive={(objectID) => store.archiveStore.archivedStoryIds.push(objectID)}
+    />,
+    document.getElementById('root')
+  );
+}
+
+autorun(render);
+
 registerServiceWorker();
