64b888b1214e30cd2a0a1c68e2977d1e6fe70d9d
Robin Wieruch <wrobin@gmx.net>
2017-09-01 15:31:54 +0200

    part 06

--- src/index.js
+++ src/index.js
@@ -2,28 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import storyStore from './stores/storyStore';
 import registerServiceWorker from './registerServiceWorker';
 
-const stories = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  }, {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 ReactDOM.render(
-  <App stories={stories} onArchive={(objectID) => { console.log(objectID); }} />,
+  <App stories={storyStore.stories} onArchive={(objectID) => { console.log(objectID); }} />,
   document.getElementById('root')
 );
 registerServiceWorker();
---
+++ src/stores/storyStore.js
@@ -0,0 +1,27 @@
+import { observable } from 'mobx';
+
+const INITIAL_STATE = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  }, {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+class StoryStore {
+  @observable stories = INITIAL_STATE;
+}
+
+const storyStore = new StoryStore();
+
+export default storyStore;
