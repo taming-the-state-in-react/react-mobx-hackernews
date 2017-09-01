e6ec828b7413ca8750a143da3f657603cefb3b8f
Robin Wieruch <wrobin@gmx.net>
2017-09-01 11:12:49 +0200

    part 00

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'mobx-react';
-import App from './components/App';
-import stores from './stores';
 import './index.css';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <Provider { ...stores }>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
